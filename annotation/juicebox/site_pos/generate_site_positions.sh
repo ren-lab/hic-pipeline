e3ee0ee1d543a85836bf8ab18c07d468c069a03f
shawnzhangyx <zhangyx.shawn@gmail.com>
2021-05-14 15:11:50 -0700

    updates

--- annotation/juicebox/site_pos/generate_site_positions.sh
+++ annotation/juicebox/site_pos/generate_site_positions.sh
@@ -1,8 +1,9 @@
-for ref in mm9 mm10 hg18 hg19; do
+for ref in hg38; do #mm9 mm10 hg18 hg19; do
   for s in HindIII DpnII AAGCTT GATC; do
     if ! [ -e "${ref}_$s.txt" ]; then
       echo $ref $s
-      ./generate_site_positions.pl $s /mnt/raid1/IMPORT/Illumina/bowtie_indexes/$ref.fa | sed 's/^chrM/chrMT/' | sed 's/^chr//' > ${ref}_$s.txt
+#      ./generate_site_positions.pl $s /mnt/raid1/IMPORT/Illumina/bowtie_indexes/$ref.fa | sed 's/^chrM/chrMT/' | sed 's/^chr//' > ${ref}_$s.txt
+       ./generate_site_positions.pl $s /projects/ps-renlab/share/bowtie2_indexes/$ref.fa  > ${ref}_$s.txt
     fi
   done
 done
